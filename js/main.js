9eae1cff997b5fcce10962f2c392474a5683fd4e
George Mintah <george.mintah@amalitech.org>
2020-10-09 11:52:24 +0100

    feat: add an item remove button, style: add float to move new items to the left

--- js/main.js
+++ js/main.js
@@ -10,12 +10,17 @@ $(document).ready(function(){
         html += '<img src="assets/accra.jpg"></img>';
         html += '<div class="description">Founded in the 17th century by the Ga people, Accra became the capital of the British Gold Coast in 1877. Following Ghana/s independence in 1957, Accra became the capital of the newly independent state</div>';
         html += '<div class="price">300</div>';
-        html += '<button> Add to cart</button>';
+        html += '<button class="item-add">Add to cart</button>';
+        html += '<button class="item-remove">Remove</button>'
         html += '<br/>';
         html += '<a href="#">More info</a>';
         html += '<div class=""more-info"> Accra is the capital of Ghana </div>';
         html += '</div>';
 
-        $('#container').append(html);
+        $('#container').prepend(html);
  });
+
+        $('#container').on('click','.item-remove',function(){
+          $(this).parent().remove();
+        })
 });
